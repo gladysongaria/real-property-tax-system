57ca06a78e1ec9629287ac4157afd2f59b725220
Jason <84706592+torogitech@users.noreply.github.com>
2025-01-15 09:32:50 +0800

    Add files via upload

---
+++ resources/views/tax_payment/receipt.blade.php
@@ -0,0 +1,120 @@
+<div class="modal fade" id="paymentreceipt" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header" style="background: #459b96;">
+                <h1 class="modal-title fs-5" id="exampleModalLabel"><strong>PAY TAXES</strong></h1>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                    <center><h5>UNOFFICIAL RECEIPT OF THE REPUBLIC OF THE PHILIPPINES</h5></center>
+                    <center><h5>Provincial or City Treasurer's Real Property Tax Receipt</h5></center>
+                    <div class="row">
+                       <p>Details</p> 
+                            <div class="container">
+                                    <div class="row">
+                                        <div class="col-6 offset-sm-1">
+                                            O.R. Date :<strong> December 05, 2024</strong> <br>
+                                            O.R. No: <strong>12356854</strong>
+                                            </div>
+                                            <div class="col-4">
+                                                Issued to:<strong> Sample </strong> <br>
+                                                Issued by: <strong>Cruz, dela Juan</strong>
+                                            </div>
+                                    </div><!--end of row2-->
+                            </div>
+                    </div>
+                    <div class="row">
+                        <strong>Payment: <input type="text"></strong>
+                    </div>
+                    <br>
+                    <br>
+                    <h4>Particulars</h4>
+                    <table class="table table-bordered">
+                        <tr>
+                        <th>Owner Name</th>
+                        <th>Barangay</th>
+                        <th>Location</th>
+                        <th>Classification</th>
+                        <th>Tax Dec</th>
+                        <th>Term</th>
+                        <th>Asessed Value</th>
+                        <th>Tax Due</th>
+                        <th>Payment</th>
+                        </tr>
+                        <tr>
+                        <td>Ayochok, Dennis D. </td>
+                        <td>Bontoc Ili</td>
+                        <td></td>
+                        <td>Agriculture</td>
+                        <td>09-0016-01139</td>
+                        <td>1991</td>
+                        <td>8820</td>
+                        <td style="text-align: right">88.2 
+                            <br>
+                            + 21.16
+                         <hr>
+
+                            <strong>Total: 109.36</strong>
+                        </td>
+                        <td></td>
+                        
+                        </tr>
+                        <tr>
+                        <td>Ayochok, Dennis D. </td>
+                        <td>Bontoc Ili</td>
+                        <td></td>
+                        <td>Agriculture</td>
+                        <td>09-0016-01139</td>
+                        <td>1991</td>
+                        <td>8820</td>
+                        <td style="text-align: right">88.2 
+                            <br>
+                            + 21.16
+                         <hr>
+
+                            <strong>Total: 109.36</strong>
+                        </td>
+                        <td rowspan="4" style="border-top: none !important">
+                            Basic: 110.47 <br>
+                        SEF: 110.47<br>
+                    Total: 220.94</td>
+                        </tr>
+                        <tr>
+                            <td colspan="8" style="text-align: right"><strong>Over all Total: 209.36</strong></td>
+                        </tr>
+                    </table>
+                    <button class="btn btn-sm btn-success">Save and Print</button>  <button class="btn btn-sm">Back</button>
+
+                    <div class="card">
+                        <div class="card-body">
+                           <div class="container">
+                            <h5>Previous Records</h5>
+                            <table class="table table-bordered">
+                                <tr>
+                                <th>TD#</th>
+                                <th>OR#</th>
+                                <th>Date Issued*</th>
+                                <th>Issued to</th>
+                                <th>Payment*</th>
+                                <th>Balance</th>
+                                <th>sdf</th>
+                                </tr>
+                                <tr>
+                                <td>09-0016-01139</td>
+                                <td>63943303</td>
+                                <td>2023-03-07 </td>
+                                <td>Dennis D. Ayochok</td>
+                                <td>158.97</td>
+                                <td>0.00</td>
+                                <td><a href="#">+ more</a></td>
+                                </tr>
+                            </table>
+                           </div>
+                        </div>
+                    </div><!--end of card-->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--END OF PAY TAXES MODAL-->
