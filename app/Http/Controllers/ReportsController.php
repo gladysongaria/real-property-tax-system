c36d4c3a8a7f56816ecc364a8aaf7fb0799e9d29
Jason <84706592+torogitech@users.noreply.github.com>
2025-03-10 06:35:45 +0800

    Update ReportsController.php

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -12,5 +12,34 @@ class ReportsController extends Controller
        return view('reports.index');
     }
 
+    public function certificate_of_payment()
+    {
+       return view('reports.certificates.certificateOfPayment');
+    }
+
+    public function printcertificate_of_payment()
+    {
+       return view('reports.certificates.printCertOfPayment');
+    }
+    
+     public function blgf()
+    {
+       return view('reports.blgf.blgf');
+    }
+
+     public function delinquency()
+    {
+       return view('reports.delinquency.delinquencyIndex');
+    }
+
+     public function taxroll()
+    {
+       return view('reports.taxroll.taxrollIndex');
+    }
+
+     public function brgyShare()
+    {
+       return view('reports.brgyshare.brgyShareIndex');
+    }
    
 }
