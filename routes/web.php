87ce0a107930f2dfc0a8d794ca5f23cce5399b0c
Jason <84706592+torogitech@users.noreply.github.com>
2025-03-10 06:36:53 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,12 @@ Route::middleware('auth', 'verified')->group(function () {
     Route::resource('penalties', PenaltyController::class);
     Route::get('transactions', [TransactionsController::class, 'index'])->name('transactions.index');
     Route::get('reports', [ReportsController::class, 'index'])->name('reports.index');
+    Route::get('certificate_of_payment', [ReportsController::class, 'certificate_of_payment'])->name('reports.certificateOfPayment');
+    Route::get('print_certificate_of_payment', [ReportsController::class, 'printcertificate_of_payment'])->name('reports.printcertificateOfPayment');
+    Route::get('blgf', [ReportsController::class, 'blgf'])->name('reports.blgf');
+    Route::get('delinquency', [ReportsController::class, 'delinquency'])->name('reports.delinquency');
+     Route::get('barangay_share', [ReportsController::class, 'brgyShare'])->name('reports.brgyShare');
+    Route::get('taxroll', [ReportsController::class, 'taxroll'])->name('reports.taxrollIndex');
     Route::get('/tax-payments', [TaxPaymentController::class, 'index'])->name('taxpayment.index');
     Route::post('/tax-payments/store', [TaxPaymentController::class, 'store'])->name('taxpayment.store');
     Route::post('/tax-payments/pay-taxes', [TaxPaymentController::class, 'payTaxes'])->name('tax-payments.pay-taxes');
